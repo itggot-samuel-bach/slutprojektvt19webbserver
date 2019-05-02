eecc8b8870780e27049d277619d92b9233b50880
Ove Sundberg <samuel.bach@itggot.se>
2019-05-02 20:49:16 +0200

    Färdigställt MVC
    Har nu färdigställt MVC för mina nuvarande funktioner och fått detta att fungera på ett effektivt och bra sätt.

--- app.rb
+++ app.rb
@@ -9,18 +9,11 @@ require_relative 'functions/functions'
 enable :sessions
 
 before() do
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-    
-    @categories = db.execute('SELECT * FROM tags')
+    start(@categories)
 end
 
 get('/') do
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-    
-    result = db.execute('SELECT * FROM posts')
-
+    result = home()
     slim(:index, locals:{posts: result, session: session})
 end
 
@@ -28,21 +21,13 @@ get('/denied') do
     slim(:denied)
 end
 
-
 get('/accepted') do
     slim(:accepted, locals:{session: session})
 end
 
 post('/login') do
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-    
-    result = db.execute("SELECT id, password FROM users WHERE username=?", [params["username"]])
-    if result.length == 0
-        redirect('/denied')
-    end  
-    if BCrypt::Password.new(result[0]["password"]) == params["password"]
-        session["user_id"] = result[0]['id']
+    state = login(params, session)
+    if state == true
         redirect('/accepted')
     else
         redirect('/denied')
@@ -50,54 +35,36 @@ post('/login') do
 end 
 
 post('/create') do
-    #Ansluta till db
     session["user_id"] = create(params)
     redirect('/')
 end
 
 get('/profile/:id') do
-
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-
-    result = db.execute('SELECT * FROM posts WHERE userId=?', params["id"])
-    user = db.execute('SELECT * FROM users WHERE id=?', params["id"])[0]
-    
-    slim(:user, locals:{posts: result, session: session, user: user})
-
+    array_values = profile(params)
+    result = array_values[0]
+    user = array_values[1]
+    my_tags = array_values[2]
+    slim(:user, locals:{posts: result, session: session, tags: my_tags, user: user})
 end
 
 get('/tags/:id/:name') do
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-
-    result = db.execute('SELECT * FROM posts WHERE tag=?', params["id"])
-
+    result = tags(params)
     slim(:topics, locals:{posts: result})
 end
 
 get('/profile/:id/edit') do
 
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-    
-    result = db.execute('SELECT * FROM users WHERE id=?', session["user_id"])
+    result = get_profile_edit(session)
 
     if session["user_id"] == params["id"].to_i
         slim(:user_edit, locals:{user: result[0]})
     else
         slim(:denied)
     end
-
 end
 
 post('/profile/:id/edit') do 
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-
-    hashed_password = BCrypt::Password.create(params["password"])
-    db.execute("REPLACE INTO users (id, username, password) VALUES (?, ?, ?)", [params["id"], params["username"], hashed_password])
-
+    post_profile_edit(params)
     redirect('/')
 end
 
@@ -108,43 +75,21 @@ end
 
 post('/post') do
     post(params, session)
-
     redirect('/')
-
 end
 
 post('/delete') do
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-
-    db.execute("DELETE FROM posts WHERE id = ?", params["post_id"])
-
+    delete(params)
     redirect('/')
 end
 
-# get('/edit/:id') do
-#     slim(:edit, locals:{id: params["id"]})
-# end
+ get('/edit/:id') do
+     slim(:edit, locals:{id: params["id"]})
+ end
 
 post('/edit_post/:id') do
-    id = params["post_id"]
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-   
-    if params.key?("image") and params["image"]
-        temp_file = params["image"]["tempfile"]
-        new_file_name = SecureRandom.uuid
-        path = File.path(temp_file)
-        new_file = FileUtils.copy(path, "./public/img/#{new_file_name}")
-        db.execute("UPDATE posts SET picture=? WHERE id=?", new_file_name, id)
-    end
-    
-    if params.key?("content") and params["content"].length > 1
-        db.execute("UPDATE posts SET content=? WHERE id=?", [params["content"], id])
-    end
-   
+    edit_post(params)
     redirect('/')
-
 end
 
 get('/comments/:id') do 
--- functions/functions.rb
+++ functions/functions.rb
@@ -1,35 +1,109 @@
 def post(params, session)
     text = params["content"]
     db = SQLite3::Database.new('db/database.db')
-
     username = db.execute("SELECT username FROM users WHERE id=?", [session["user_id"]])
-
     new_file_name = SecureRandom.uuid
     temp_file = params["image"]["tempfile"]
     path = File.path(temp_file)
     tag = params["tag"]
     tag_id = db.execute("SELECT id FROM tags WHERE name=?", tag)[0]
-
     new_file = FileUtils.copy(path, "./public/img/#{new_file_name}")
-
     db.execute('INSERT INTO posts (content, picture, userId, tag, author) VALUES (?, ?, ?, ?, ?)', [text, new_file_name, session['user_id'], tag_id, username])
 end
 
 def create(params)
     db = SQLite3::Database.new('db/database.db')
     db.results_as_hash = true
-
     existing_user = db.execute("SELECT id FROM users WHERE username=?", [params["username"]])
-    
     if existing_user.length > 0
         redirect('/')
     end
-
     hashed_password = BCrypt::Password.create(params["password"])
     db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [params["username"], hashed_password])
-
     user_id = db.execute("SELECT id FROM users WHERE username=?", [params["username"]])
-
     return user_id[0]["id"]
 end
 
+def start(categories)
+db = SQLite3::Database.new('db/database.db')
+db.results_as_hash = true
+@categories = db.execute('SELECT * FROM tags')
+end
+
+def home()
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    result = db.execute('SELECT * FROM posts')
+    return result
+end
+
+def login(params, session)
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT id, password FROM users WHERE username=?", [params["username"]])
+    if result.length == 0
+        return false
+    end  
+    if BCrypt::Password.new(result[0]["password"]) == params["password"]
+        session["user_id"] = result[0]['id']
+        return true
+    else
+        return false
+    end
+end
+
+def profile(params)
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    result = db.execute('SELECT * FROM posts WHERE userId=?', params["id"])
+    user = db.execute('SELECT * FROM users WHERE id=?', params["id"])[0]
+    my_tags = db.execute('SELECT * FROM tags')
+    return result, user, my_tags
+end
+
+def tags(params)
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    result = db.execute('SELECT * FROM posts WHERE tag=?', params["id"])
+    return result
+end
+
+def get_profile_edit(session)
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    result = db.execute('SELECT * FROM users WHERE id=?', session["user_id"])
+    return result
+end
+
+def post_profile_edit(params)
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    hashed_password = BCrypt::Password.create(params["password"])
+    db.execute("REPLACE INTO users (id, username, password) VALUES (?, ?, ?)", [params["id"], params["username"], hashed_password])
+
+end
+
+def delete(params)
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    db.execute("DELETE FROM posts WHERE id = ?", params["post_id"])
+end
+
+def edit_post(params)
+    id = params["id"]
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+   
+    if params.key?("image") and params["image"]
+        temp_file = params["image"]["tempfile"]
+        new_file_name = SecureRandom.uuid
+        path = File.path(temp_file)
+        new_file = FileUtils.copy(path, "./public/img/#{new_file_name}")
+        db.execute("UPDATE posts SET picture=? WHERE id=?", new_file_name, id)
+    end
+
+    if params.key?("content") and params["content"].length > 1
+        db.execute("UPDATE posts SET content=? WHERE id=?", [params["content"], id])
+    end
+   
+end
