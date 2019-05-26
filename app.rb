1007efe53ada2115ff69e9c4dfc3f7a6c51b46a3
Ove Sundberg <samuel.bach@itggot.se>
2019-05-26 22:03:02 +0200

    Final version with documentation
    The final version is now finished and it also included a finalised documentation.

--- app.rb
+++ app.rb
@@ -9,10 +9,9 @@ include Model
 enable :sessions
 
 configure do 
-    set :unsecured_profile_paths, ["/profile/:id/edit"]
-    set :unsecured_post_paths, ["/edit/:id", "/edit_post/:id", "/delete"]
+    set :unsecured_profile_paths, [/\/profile\/\d+\/edit/]
+    set :unsecured_post_paths, [/\/edit\/\d+/, /\/edit_post\/\d+/, /\/delete/]
 end
-
 
 set(:auth) do |*args|
     condition do
@@ -21,26 +20,28 @@ set(:auth) do |*args|
             redirect('/')
         end
         settings.unsecured_profile_paths.each do |unsecured|
-            if unsecured == request.path
-                if session["id"] != params['id'].to_i
+            if !(unsecured =~ request.path).nil?
+                if session["user_id"] != params['id'].to_i
                     session[:error] = "You are not this user, stay away!"
-                    return false
+                    redirect('/')
                 end
             end
         end
         settings.unsecured_post_paths.each do |unsecured|
-            if unsecured == request.path
+            if !(unsecured =~ request.path).nil?
                 owner = post_owner(params)
                 if session["user_id"] != owner
                     session[:error] = "You are not the owner of this post!"
-                    return false
+                    redirect('/')
                 end
             end
         end
-    return true
+        return true
     end
 end
-
+# Loads all the tags on a specific page
+#
+# @see Model#start
 get('/tags') do
     tags = start()
     slim(:tags, locals: {tags: tags})
@@ -48,12 +49,13 @@ end
 
 # Display Landing Page
 #
+# @see Model#home
 get('/') do
     result = home()
     slim(:index, locals:{posts: result, session: session})
 end
 
-#Loads the denied page
+# Loads the denied page
 #
 get('/denied') do
     slim(:denied)
@@ -65,11 +67,12 @@ get('/accepted') do
     slim(:accepted, locals:{session: session})
 end
 
-#Attempts login and updates the session
+# Attempts login and updates the session["user_id"] if a login is sucessfull. If it is unsucessfull it will display an error.
 #
 # @param [String] username, The Username
 # @param [String] password, the Password
-# @see Model#Login
+# @param [String] password2, the Password repeated
+# @see Model#login
 post('/login') do
     state = login(params)
     if state.key?(:user_id)
@@ -82,12 +85,12 @@ post('/login') do
     end
 end 
 
-#Attempts to create a user and logs in if successfull
+# Creates a user if there is not an already existing user with the same name. If another user exists an error message will be display.
 #
 # @param [String] username, The Username
-# @param [String] password, The Password
-#
-# @see Model#Create
+# @param [String] password, the Password
+# @param [String] password2, the Password repeated
+# @see Model#create
 post('/create') do
     state = create(params)
     if state.key?(:user_id)
@@ -100,8 +103,10 @@ post('/create') do
     end
 end
 
-#Loads the profile page
+# Loads the profile page if a user with such an id requested exists. If a user does not exist an error will be displayed.
 #
+# @param [Integer] id, The id of the user
+# @see Model#profile
 get('/profile/:id') do
     result = profile(params)    
     if result.key?(:user)
@@ -113,10 +118,10 @@ get('/profile/:id') do
     end
 end
 
-#Loads the specific tags page and displays it with an id and name of the tag
+# Loads the specific tags page and displays it with an id and name of the tag. If the tag id does not exist an error will be displayed.
 #
-# param [Integer]
-#
+# @param [Integer] id, The id of the tag
+# @see Model#tags
 get('/tags/:id/:name') do
     result = tags(params)
     if result.key?(:posts)
@@ -127,11 +132,10 @@ get('/tags/:id/:name') do
     end
 end
 
-#Edits the currently logged in user's credentials
+# Edits the currently logged in user's credentials if the logged in user is the owner of the attempted user being edited. If not, an error will be displayed.
 #
-# @param [integer] id, user's id
-#
-# @see Model#GetProfileEdit
+# @param [Integer] id, The user's id of which is being attempted to edit. 
+# @see Model#get_profile_edit
 get('/profile/:id/edit', auth: true) do
 
     result = get_profile_edit(session, params)
@@ -144,13 +148,13 @@ get('/profile/:id/edit', auth: true) do
     end
 end
 
-#Implements the changes done to the logged in user's credentials
+# Implements the changes done to the logged in user's credentials if the previous request was sucessfull. 
 #
 # @param [integer] id, user's id
 # @param [string] username, The Username
 # @param [string] password, The Password
 #
-# @see Model#PostProfileEdit
+# @see Model#post_profile_edit
 post('/profile/:id/edit', auth: true ) do 
     result = post_profile_edit(params)
     if result.key?(:error)
@@ -167,9 +171,12 @@ post('/logout') do
     redirect('/')
 end
 
-#Posts a post by a user
+# Posts a post by a user if the user is logged in. If a user is not logged in, an error will be displayed.
 #
-# @param []
+# @param [String] tags, the name of the tag.
+# @param [String] image, the image in raw data. 
+# @param [String] content, the raw text of the post. 
+# @see Model#post
 post('/post') do
     if session["user_id"]
         state = post(params, session['user_id'])
@@ -179,22 +186,32 @@ post('/post') do
     end 
     redirect('/')
 end
-
+# Deletes a post if the user is the owner of the post and logged in. If the statements are not fulfilled, an error will show up. 
+#
+# @param [Integer] id, the id of the post.
+# @see Model#delete
 post('/delete', auth: true) do
     delete(params, session)
     redirect('/')
+end 
+
+# Calls for the page that allows one to edit a post. If the user is not logged in or does not own the right to edit the post, an error will be displayed.
+#
+# @param [Integer] id, the id of the post.
+# @param [String] image, the image in raw data. 
+# @param [String] content, the raw text of the post. 
+# 
+get('/edit/:id', auth: true) do
+    slim(:edit, locals:{id: params["id"]})
 end
 
- get('/edit/:id', auth: true) do
-     slim(:edit, locals:{id: params["id"]})
- end
 
-
- # Description
- # 
- # @param [Integer] :id, Description.
- # 
- # @see Model#Function
+# Completes the action request before and once again checks if the user is logged in on the owner of the post's account. If not, an error will be displayed.
+# 
+# @param [Integer] id, the id of the post.
+# @param [String] image, the image in raw data. 
+# @param [String] content, the raw text of the post. 
+# @see Model#edit_post
 post('/edit_post/:id', auth: true) do
     edit_post(params)
     redirect('/')
--- functions/functions.rb
+++ functions/functions.rb
@@ -1,4 +1,15 @@
 module Model
+    # Gathers all the input information from the user and then inserts it into the database as a post. 
+    #
+    # @param [Hash] params form data
+    # @option params [String] content The text of the posts
+    # @option params [Image] image The post's image
+    # @option params [String] tags The name of the tags selected
+    #
+    # @return [Hash] returns an empty hash
+    #
+    # @return [Hash] contains an error message
+    #   * :error [String] The error message
     def post(params, user_id)
         if validate_create_post(params)
             text = params["content"]
@@ -26,6 +37,16 @@ module Model
         end
     end
 
+    # Attempts to create a user if all the necessary data has been provided.
+    #
+    # @param [Hash] params form data
+    # @option params [String] username The username from the user's input
+    # @option params [String] password The password from the user's input
+    #
+    # @return [Hash] contains the users id
+    #   * :user_id [Integer] The user's id
+    # @return [Hash] contains an error message
+    #   * :error [String] The error message
     def create(params)
         if validate_create_user(params)
             db = connect_to_db()
@@ -39,18 +60,24 @@ module Model
             hashed_password = BCrypt::Password.create(params["password"])
             db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [params["username"], hashed_password])
             user_id = db.execute("SELECT id FROM users WHERE username=?", [params["username"]])
-            return {user_id: result[0]['id']}
+            return {user_id: user_id[0]['id']}
         else
             return {error: "Your username or password is too long."}
         end
     end
 
+    # Loads all the tags and displays them on a specific page once requested.
+    #
+    # @return [Array] contains all tags
     def start()
         db = connect_to_db()
         db.results_as_hash = true
         db.execute('SELECT * FROM tags')
     end
 
+    # Loads all the posts and displays them on the index page
+    #
+    # @return [Array] contains all the posts
     def home()
         db = connect_to_db()
         db.results_as_hash = true
@@ -59,6 +86,18 @@ module Model
         return result
     end
 
+    # Logs in the user if the user input provided is correct.
+    #
+    # @param [Hash] params form data
+    # @option params [String] username The username from the user's input
+    # @option params [String] password The password from the user's input
+    #
+    # @return [Hash] contains the users id
+    #   * :user_id [Integer] The user's id
+    # @return [Hash] contains the first error message
+    #   * :error [String] The first error message
+    # @return [Hash] contains the second error message
+    #   * :error [String] The second error message
     def login(params)
         if validate_login_user(params)
             db = connect_to_db()
@@ -73,11 +112,20 @@ module Model
                 return {error: "You have not provided the correct credentials!"}
             end
         else
-            return {error: "Not valid input"}
+            return {error: "Make sure your password is the same in both boxes."}
         end
     end
 
-
+    # Attempts to load a specific user's profile page
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The id of the user requested to visit
+    #
+    # @return [Hash] contains the users username and password and the posts owned by the user
+    #   * :posts [Array] The posts owned by the user
+    #   * :user [Integer] The user's username and password
+    # @return [Hash] contains an error message
+    #   * :error [String] The error message
     def profile(params)
         if validate_profile(params)
             db = connect_to_db()
@@ -92,6 +140,15 @@ module Model
         end
     end
 
+    # Loads the specific page of a specificed tag and all the posts associated with it.
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The id of the specified tag
+    #
+    # @return [Hash] contains posts associated with the specified tag
+    #   * :posts [Array] The posts associated with the specified tag
+    # @return [Hash] contains an error message
+    #   * :error [String] The error message
     def tags(params)
         if validate_tags(params)
             db = connect_to_db()
@@ -103,6 +160,15 @@ module Model
         end
     end
 
+    # Gets the form for editing a specific profile.
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The id of the specified user requested to edit
+    #
+    # @return [Hash] contains the user's username and password with of the one currently logged in.
+    #   * :user [Array] The username and password of the currently logged in user
+    # @return [Hash] contains an error message
+    #   * :error [String] The error message
     def get_profile_edit(session, params)
         if validate_profile_edit_get(session, params)
             db = connect_to_db()
@@ -110,10 +176,21 @@ module Model
             result = db.execute('SELECT * FROM users WHERE id=?', session["user_id"])
             return {user: result}
         else
-            return {error: "You are not logged in"}
+            return {error: "You are not this user!"}
         end
     end
 
+    # Conducts the requested changes to the profile from the get_profile_edit function.
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The id of the specified user requested to edit
+    # @option params [String] username The username of the specified user requested to edit
+    # @option params [String] password The password of the specified user requested to edit
+    #
+    # @return [Hash] returns an empty hash
+    #
+    # @return [Hash] contains an error message
+    #   * :error [String] The error message
     def post_profile_edit(params)
         if validate_profile_edit_post(params)
             db = connect_to_db()
@@ -122,10 +199,19 @@ module Model
             db.execute("REPLACE INTO users (id, username, password) VALUES (?, ?, ?)", [params["id"].to_i, params["username"], hashed_password])
             return {}
         else
-            return {error: "You are not logged in"}
+            return {error: "You are not this user!"}
         end
     end
 
+    # Deletes a specified post.
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The id of the specified post requested to deleted
+    #
+    # @return [true] Returns true if the post is allowed to be and is deleted.
+    # 
+    # @return [false] Returns false if the post is not allowed to be deleted
+    #
     def delete(params, session)
         if validate_delete(params, session)
             db = connect_to_db()
@@ -137,6 +223,17 @@ module Model
         end
     end
 
+    # Edits a post specified by the user's input.
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The id of the specified post requested to edit
+    # @option params [Image] image The image for the post it is requested to change for
+    # @option params [String] content The content the post is requested to change it to
+    #
+    # @return [Array] Returns the updated post's information
+    #
+    # @return [Hash] contains an error message
+    #   * :error [String] The error message
     def edit_post(params)
         if validate_edit_post(params) 
             id = params["id"]
@@ -159,6 +256,15 @@ module Model
         end
     end
 
+    # Checks whoever owns a specified post
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The id of the specified post
+    #
+    # @return [Integer] Returns the user who owns the specified post
+    #
+    # @return [false] Returns false if not enough information is provided to determine the owner
+    #
     def post_owner(params)
         if validate_post_owner(params)
             db = connect_to_db()
@@ -171,22 +277,67 @@ module Model
         end
     end
 
+    # A function that defines which database is being used. 
+    #
+    # @return [SQLite3::Database] Returns the specified database
+    #
     def connect_to_db()
         SQLite3::Database.new('db/database.db')
     end
 
+    # Checks that all the necessary data and conditions are met to allow the creation of the post
+    #
+    # @param [Hash] params form data
+    # @option params [String] content The text in the post
+    # @option params [String] tags The tag's name in the post
+    # @option params [Image] image The image in the post
+    #
+    # @return [true] Returns true if the data and conditions are met
+    #
+    # @return [false] Returns false if the data and conditions are not met
+    #
     def validate_create_post(params)
         params["content"].length < 500 and params["tags"] and params["image"]
     end
 
+    # Checks that all the necessary data and conditions are met to allow the creation of a user
+    #
+    # @param [Hash] params form data
+    # @option params [String] username The provided username
+    # @option params [String] password The provided password
+    #
+    # @return [true] Returns true if the data and conditions are met
+    #
+    # @return [false] Returns false if the data and conditions are not met
+    #
     def validate_create_user(params)
         params["username"].length < 25 and params["password"].length < 25
     end
     
+    # Checks that all the necessary data and conditions are met to allow the user to login
+    #
+    # @param [Hash] params form data
+    # @option params [String] username The provided username
+    # @option params [String] password The provided password
+    # @option params [String] password2 A second box with the hopefully same provided password as the first box
+    #
+    # @return [true] Returns true if the data and conditions are met
+    #
+    # @return [false] Returns false if the data and conditions are not met
+    #
     def validate_login_user(params)
         params["username"] and params["password"] and params["password"] == params["password_2"]
     end
 
+    # Checks wether or not there is a user with the specified id
+    #
+    # @param [Hash] params form data
+    # @option params [String] id The specified user id to check
+    #
+    # @return [true] Returns true if there is a user with the specified id
+    #
+    # @return [false] Returns false if there is not a user with the specified id
+    #
     def validate_profile(params)
         db = connect_to_db()
         db.results_as_hash = true
@@ -200,6 +351,15 @@ module Model
         end
     end
 
+    # Checks wether or not there is a tag with the specified id
+    #
+    # @param [Hash] params form data
+    # @option params [String] id The specified tag id to check
+    #
+    # @return [true] Returns true if there is a tag with the specified id
+    #
+    # @return [false] Returns false if there is not a gag with the specified id
+    #
     def validate_tags(params)
         db = connect_to_db()
         db.results_as_hash = true
@@ -212,7 +372,16 @@ module Model
             return false
         end
     end
-
+    
+    # Checks wether or not the logged in user is the user of the profile requested to edit
+    #
+    # @param [Hash] params form data
+    # @option params [String] id The specified user id to check
+    #
+    # @return [true] Returns true if the logged in user is the user of the profile requsted to edit
+    #
+    # @return [false] Returns false if the logged in user is not the user of the profile requsted to edit
+    #
     def validate_profile_edit_get(session, params)
         db = connect_to_db()
         db.results_as_hash = true
@@ -226,10 +395,30 @@ module Model
         end
     end
 
+    # Checks that all the necessary data available to allow the changes to the user to go through
+    #
+    # @param [Hash] params form data
+    # @option params [String] username The new username
+    # @option params [String] password The new password
+    # @option params [Integer] id The user's id
+    #
+    # @return [true] Returns true if the data and conditions are met
+    #
+    # @return [false] Returns false if the data and conditions are not met
+    #
     def validate_profile_edit_post(params)
-        params["password"] and params["id"] and params["username"]
+        params["username"] and params["password"] and params["id"]
     end
 
+    # Checks wether or not the logged in user is the owner of the post requested to delete
+    #
+    # @param [Hash] params form data
+    # @option params [String] id The specified user id to check
+    #
+    # @return [true] Returns true if the logged in user is the owner of the post requsted to delete
+    #
+    # @return [false] Returns false if the logged in user is not the owner of the post requsted to delete
+    #
     def validate_delete(params, session)
         db = connect_to_db()
         db.results_as_hash = true
@@ -243,10 +432,30 @@ module Model
         end
     end
 
+    # Checks that all the necessary data available to allow the editing of a post
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The post's id
+    # @option params [Image] image The new image for the post
+    # @option params [String] content The new text in the post
+    #
+    # @return [true] Returns true if the data is available
+    #
+    # @return [false] Returns false if the data is not available
+    #
     def validate_edit_post(params)
         params["id"] and params["image"] and params["content"] 
     end
 
+    # Checks that all the necessary data available to determine who's the owner of a specified post
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The post's id
+    #
+    # @return [true] Returns true if the data is available
+    #
+    # @return [false] Returns false if the data is not available
+    #
     def validate_post_owner(params)
         if params["id"]
             return true
